27cc06e7cb4b32ce5e10324bcfc00dd0969c12e6
RodJunio94 <jtech.rodrigues@gmail.com>
2024-12-26 16:12:19 -0300

    Documentação da API

--- src/Services/Identidade/NSE.Identidade.API/Program.cs
+++ src/Services/Identidade/NSE.Identidade.API/Program.cs
@@ -19,7 +19,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { 
+        Title = "NerdStore Enterprise identity API",
+        Description ="Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
+        Contact = new() { Name = "Junio Rodrigues", Email = "devjuniorodrigues@gmail.com"},
+        License = new() { Name = "MIT", Url = new("https://opensource.org/licenses/MIT") },
+        Version = "v1" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NSE.Identidade.API v1"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
